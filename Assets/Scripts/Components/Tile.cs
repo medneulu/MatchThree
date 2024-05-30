fbc14095745b7c13c65edf44f677fc1bdb256441
mdnulu25@hotmail.com <mdnulu25@hotmail.com>
2024-05-30 14:26:57 +0300

    init push

---
+++ Assets/Scripts/Components/Tile.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+namespace Components
+{
+    [Serializable]
+    public class Tile:MonoBehaviour
+    {
+        [SerializeField] private Vector2Int _coords;
+        public Vector2Int Coords => _coords;
+        [SerializeField] private int _id;
+        public int ID => _id ;
+
+        public void Construct(Vector2Int coords)
+        {
+            _coords = coords;
+        }
+
+    }
+}
